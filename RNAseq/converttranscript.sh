abf6e4ba54ae6bcdfd8c53a18b306620bb52c032
Kh0iHuynh <srongthiencos@gmail.com>
2024-06-04 11:00:15 -0700

    Update converttranscript.sh

--- RNAseq/converttranscript.sh
+++ RNAseq/converttranscript.sh
@@ -3,6 +3,17 @@
 #####
 # convert trasncript name to only gene name
 #####
-cut -f1 -d" " anova_normalized_count_result.txt | cut -f1 -d"." > delete.txt
-cut -f2,3,4,5 -d" " anova_normalized_count_result.txt > delete2.txt
-paste -d" " delete.txt delete2.txt > deseq_anova_result.txt
+cut -f1 -d" " deseq_result_cohort.txt | cut -f1 -d"." > delete.txt
+cut -f2,3,4,5,6,7 -d" " deseq_result_cohort.txt > delete2.txt
+paste -d" " delete.txt delete2.txt > deseq_result_cohort_ensgene.txt
+
+
+
+cut -f1 -d" " deseq_result_survival.txt | cut -f1 -d"." > delete.txt
+cut -f2,3,4,5,6,7 -d" " deseq_result_survival.txt > delete2.txt
+paste -d" " delete.txt delete2.txt > deseq_result_survival_ensgene.txt
+
+
+cut -f1 -d" " deseq_result_interaction.txt | cut -f1 -d"." > delete.txt
+cut -f2,3,4,5,6,7 -d" " deseq_result_interaction.txt > delete2.txt
+paste -d" " delete.txt delete2.txt > deseq_result_interaction_ensgene.txt
